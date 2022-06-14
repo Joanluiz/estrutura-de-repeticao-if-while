6949a1641bd493d59250a3f0397cfb33b384b893
Joanluiz <joanluiz69@gmail.com>
2022-06-14 11:47:46 -0300

    rename

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1 - Criar um loop que conte de 1 até 10 usando FOR
 
-for(let count = 1; count <= 10; count++) {
-    console.log(count);
+for(let loop = 1; loop <= 10; loop++) {
+    console.log(loop);
 };
 
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
@@ -15,8 +15,8 @@ while(1 <= count){
 
 //3 - Criar um loop que conte todos os números ímpares de 1 até 100
 
-for (let impares = 1; impares <= 100; impares +=2){
-    console.log(impares);
+for (let impar = 1; impar <= 100; impar +=2){
+    console.log(impar);
 };
 
 //4- Criar um loop que conte todos os números pares de 0 a 100
